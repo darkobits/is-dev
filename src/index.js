c7690005ee01d9330da76d1138d85c1fa9fac54b
Joshua Martin <joshua@collectivehealth.com>
2017-11-30 14:11:53 -0800

    refactor: Simplify algorithm.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-export {
-  default
-} from './lib/is-dev';
+export {default} from './lib/is-dev';
--- src/lib/is-dev.js
+++ src/lib/is-dev.js
@@ -1,56 +1,19 @@
 import {resolve} from 'path';
-import findRoot from 'find-root';
-import log from './log';
-
-
-const LOG_LABEL = 'isDev';
+import callerPath from 'caller-path';
 
 
 /**
- * - If "npm install" is run from this package, return true.
- * - If "npm install" is run from this package's direct consumer, return true.
- * - If "npm install" is run from a consumer of this package's direct consumer,
- *   return false.
+ * This function operates under the assumption that if the path to a file
+ * contains "node_modules", then the calling module is acting as a dependency of
+ * another module. If the path does not contain "node_modules", then the calling
+ * module is the host package.
  *
- * @return {boolean}
+ * @param  {string}  [path] - Optional path (used internally). If not provided,
+ *   the path to the file calling this function will be used.
+ * @return {Boolean} - Returns true if the calling module is the host (ie:
+ *   cloned and set up via "npm install") and false if the calling module is a
+ *   dependency (ie: set up via "npm install some-module").
  */
-function isDev() {
-  try {
-    // This will always be us.
-    const ourRoot = findRoot(__dirname);
-    const ourName = require(resolve(ourRoot, 'package.json')).name;
-    log.silly(LOG_LABEL, `"${ourName}" root: ${ourRoot}`);
-
-    // This will be us when we are being developed locally and our dependent
-    // when they are being developed locally.
-    const dependentRoot = findRoot(resolve(process.cwd()));
-    const dependentName = require(resolve(dependentRoot, 'package.json')).name;
-
-    if (dependentName === ourName) {
-      return true;
-    }
-
-    log.silly(LOG_LABEL, `"${dependentName}" root: ${dependentRoot}`);
-
-    // This will be falsy when our dependent is being developed locally and our
-    // dependent's dependent when our dependent is being installed.
-    const hostRoot = findRoot(resolve(dependentRoot, '..'));
-
-    if (!hostRoot) {
-      log.verbose(LOG_LABEL, `${dependentName} is being installed locally.`);
-      return true;
-    }
-
-    const hostName = require(resolve(hostRoot, 'package.json')).name;
-
-    log.silly(LOG_LABEL, `"${hostName}" root: ${hostRoot}`);
-    log.verbose(LOG_LABEL, `"${dependentName}" is being installed by "${hostName}".`);
-    return false;
-  } catch (err) {
-    log.error(LOG_LABEL, err.message);
-    throw err;
-  }
+export default function isDev(path) {
+  return String(path ? resolve(path) : callerPath()).indexOf('node_modules') === -1;
 }
-
-
-export default isDev();
