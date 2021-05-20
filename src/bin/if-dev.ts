376dfa667d1a8dd91a2843624b40529831d50f23
darkobits <joshua+github@frontlawn.net>
2021-05-20 01:04:17 -0700

    fix: Use shell when invoking commands.

--- src/bin/if-dev.ts
+++ src/bin/if-dev.ts
@@ -6,8 +6,9 @@ import isDev from '../is-dev';
 
 async function main() {
   if (isDev()) {
-    await execa(process.argv.slice(2).join(' '), {
-      stdio: 'inherit'
+    await execa.command(process.argv.slice(2).join(' '), {
+      stdio: 'inherit',
+      shell: true
     });
   }
 }
